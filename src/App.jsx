476d9e5f75ef2521945fc9f9f3f63e1651dd01e6
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-13 14:32:50 +0300

    Built more reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orange;
   padding: 20px;
 `;
 
@@ -14,14 +14,30 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello World </Heading>
-        <Heading as="h2">Check in and out </Heading>
-        <Button onClick={() => alert("nbn")}>Check in</Button>
-        <Button onClick={() => alert("nbn")}>Check out</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis </Heading>
+            <div>
+              <Heading as="h2">Check in and out </Heading>
+              <Button onClick={() => alert("nbn")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("nbn")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of quests" />
-        <Input type="number" placeholder="Number of quests" />
+          <Heading as="h3">Form</Heading>
+          <Row>
+            <form>
+              <Input type="number" placeholder="Number of quests" />
+              <Input type="number" placeholder="Number of quests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
