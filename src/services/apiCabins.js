2d1cd238049a4abd76d728c62f3f89ea1290cac6
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-16 16:24:57 +0300

    Added mutations: deleting a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,6 +5,18 @@ export async function getCabins() {
 
   if (error) {
     console.log(error);
+    throw new Error("Cabins could not be loaded");
   }
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return data;
+}
+deleteCabin;
