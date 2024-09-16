76f01f00c1d9ab2a39906bcc19c53bd8349fe6de
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-16 15:53:09 +0300

    Fetched cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,18 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://rcjxtdjnwbbtimrkhlgo.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
