f0ab85b4d56a048a99b83b712ebdf5acaaeca683
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-20 17:25:26 +0300

    Built a reusable table

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -6,23 +6,15 @@ import Modal from "../../ui/Modal";
 function AddCabin() {
   return (
     <div>
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
       </Modal>
-      </div>
-
-      {/* <Modal.Open>
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window>
-        <CreateCabinForm />
-      </Modal.Window> */}
-   
+    </div>
   );
 }
 
