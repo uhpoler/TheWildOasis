2501fc28510aa8fb6e132ccacfccca98e35d69e7
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-23 13:51:24 +0300

    Uploaded sample data

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -51,7 +51,7 @@ function Filter({ filterField, options }) {
           <FilterButton
             key={option.value}
             onClick={() => handleClick(option.value)}
-            active={option.value === currentFilter}
+            active={(option.value === currentFilter).toString()}
             disabled={option.value === currentFilter}
           >
             {option.lable}
