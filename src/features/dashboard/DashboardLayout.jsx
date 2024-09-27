a49ce5ff362515c6dd9591db2fa133735b691069
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-27 11:16:35 +0300

    Computed recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useResentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,7 +9,12 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
   return (
     <StyledDashboardLayout>
       <div>Statisctic</div>
